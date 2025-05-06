8c8975051e35b9c32cbf2c168d69e4570597e8a5
carl-leonador03 <caleonador@my.cspc.edu.ph>
2025-05-06 13:47:45 +0800

    added some files and edited some files

--- app.py
+++ app.py
@@ -9,15 +9,4 @@ def index():
     """Renders the index page for the root endpoint."""
     return render_template("index.html")
 
-@app.route("/fetch/icons")
-def fetch_icons():
-    """Fetches the SVG icons located under static/assets/icons"""
-    icons = {}
-    for icon in os.listdir("static/assets/icons"):
-        with open(f"static/assets/icons/{icon}") as icon_file:
-            root = etree.fromstring(icon_file.read())
-        icons[os.path.splitext(icon)[0]] = root.find("path", root.nsmap).get("d")
-    
-    return jsonify(icons)
-
 app.run(debug = True)
