25e9852895c236e8ae12d6497d20ba53ca4361f8
user@cspc.edu.ph <you@cspc.edu.ph>
2025-05-06 16:08:17 +0800

    changes 1

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, url_for, render_template, jsonify
-import requests, os
-from lxml import etree
+from flask import Flask, url_for, render_template, jsonify, session, redirect, request
 
 app = Flask(__name__)
 
@@ -9,4 +7,30 @@ def index():
     """Renders the index page for the root endpoint."""
     return render_template("index.html")
 
-app.run(debug = True)
+@app.route("/posting", methods = ['GET', 'POST'])
+def posted_ad():
+    """
+    What is Seen: 👀👀👀
+    The Item/Services/Jobs being advertised or sold.
+    """
+    return "ad posting"
+
+@app.route("/account")
+def account_page():
+    if "username" not in session.keys() or session.get("username") != None:
+        return redirect(url_for('login_page'))
+    return "banana"
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login_page():
+    if request.method == 'GET':
+        # For fetching the 
+        return render_template('login.html')
+    elif request.method == 'POST':
+        # TODO: Do user login authentication.
+        return "obamna"
+    return "SODAAAA"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
--- static/scripts/iconify.js
+++ static/scripts/iconify.js
@@ -1,7 +1,7 @@
 // Home-made JavaScript for adding icons for specific elements
 
 /** Adds a magnifying lens icon to the search bar. */
-async function searchIcon()
+function searchIcon()
 {
     var icon = document.createElement("span");
     icon.id = "search";
@@ -10,4 +10,16 @@ async function searchIcon()
 
     const searchBox = document.querySelector('input[name="search"]');
     searchBox.parentElement.insertBefore(icon, searchBox);
+}
+
+/** Adds a person icon to the account button. */
+function accountIcon()
+{
+    var icon = document.createElement("span");
+    icon.id = "account";
+    icon.classList.add("material-symbols-outlined");
+    icon.textContent = "person";
+
+    const accountButton = document.querySelector('button#account');
+    searchBox.parentNode.insertBefore(icon, accountButton);
 }
